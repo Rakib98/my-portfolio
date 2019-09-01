884e421e642b41aaaac5faa4bbc8c9d38a7170a2
Rakib Abdur <rakibabdur98@gmail.com>
2019-09-01 14:50:14 +0100

    added folders

---
+++ src/components/Carousel.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import Card from '../components/Card';
+
+import brookfield from '../assets/img/brookfield.png';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+class Carousel extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {
+                    id: 0,
+                    title: 'Brookfield Garden & Pet Centre',
+                    subTitle: 'Simple website',
+                    imgSrc: brookfield,
+                    link: 'https://stupefied-heyrovsky-e82f27.netlify.com/',
+                    selected: false
+                }
+            ]
+        }
+    }
+
+
+    /* handleCardClick = (id, card) => {
+
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if(item.id !== id) {
+                item.selected = false;
+            }
+        });
+
+        this.setState({
+            items
+        });
+    }
+ */
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
+
+    render() {
+        return(
+            <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
+        );
+    }
+
+}
+
+export default Carousel;
