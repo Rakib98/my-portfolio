0f307b9f1201852d0b9d2f3452dae80ab0f6c8a0
Rakib Abdur <rakibabdur98@gmail.com>
2019-09-05 19:06:07 +0100

    Updated CV linkk

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -11,7 +11,7 @@ function AboutPage(props) {
                 <p>Hi, my name is Rakib Abdur, and I am a student an undergraduate in Software and Web Development. I would like to start working as a fullstack developer, with a bit more emphasize on the front-end. For this reason, I started with React, which has been used to make this website. Every web-dev needs a portfolio, so I decided to create one in React; so that I can use it to both show my skills with React, as well as use it to showcase myself, and future projects.
                 In the next months, I should upload more projects, using React, or Angular.
                 As said before, I am interested in fullstack, so for the backend I am learned a bit of PHP and MySQL. I saw that NodeJS is very popular at the moment, so I want to learn Node, and use it for dealing with requests and more.
-                You can check my CV in here: <a href="https://drive.google.com/open?id=1l6R3plg0sT-ZxT--6dFlRZj0_fQe1SmI" rel="noopener noreferrer" target="_blank">CV</a>
+                You can check my CV in here: <a href="https://drive.google.com/open?id=1DBO7lcSeBx4tHR7dJPIDmd_rj3DMWwMaJkooEvLtut4" rel="noopener noreferrer" target="_blank">CV</a>
                 </p>
 
                 <p>Check some of the work I have done at my last dev job:
