cf86e9fe824cac59e855e75bd7a66141a95808b6
Rakib Abdur <rakibabdur98@gmail.com>
2019-09-17 10:53:42 +0100

    added a new project

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import CardInfo from '../components/CardInfo';
-import Card from 'react-bootstrap/Card';
+import CardInfo from "../components/CardInfo";
+import Card from "react-bootstrap/Card";
 
 function Cards(props) {
-
-    return (
-        <div className="outside-card pr-4">
-            <Card className="mb-2">
-                <Card.Img variant="top" src={props.item.imgSrc} />
-                <Card.Body>
-                    <CardInfo title={props.item.title} subTitle={props.item.subTitle} link={props.item.link} code={props.item.code} />
-                </Card.Body>
-            </Card>
-        </div>
-    );
-
+  return (
+    <div className="outside-card pr-4">
+      <Card className="mb-3">
+        <Card.Img variant="top" src={props.item.imgSrc} />
+        <Card.Body>
+          <CardInfo
+            title={props.item.title}
+            subTitle={props.item.subTitle}
+            link={props.item.link}
+            code={props.item.code}
+          />
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
 
 export default Cards;
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,59 +1,66 @@
-import React from 'react';
+import React from "react";
 
-import Card from '../components/Card';
+import Card from "../components/Card";
 
-import brookfield from '../assets/img/brookfield.png';
-import phpShowcase from '../assets/img/phpShowcase.png';
-import walsall from '../assets/img/walsall.png';
-import pizza from '../assets/img/pizza.png';
+import brookfield from "../assets/img/brookfield.png";
+import phpShowcase from "../assets/img/phpShowcase.png";
+import walsall from "../assets/img/walsall.png";
+import pizza from "../assets/img/pizza.png";
+import lyricsApp from "../assets/img/lyricsApp.png";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 class Carousel extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [
-                {
-                    id: 0,
-                    title: 'Brookfield Garden & Pet Centre',
-                    subTitle: 'Website for a fictional gaden and pet store',
-                    imgSrc: brookfield,
-                    link: 'https://rakib-brookfield.netlify.com',
-                    code: 'https://github.com/Rakib98/Brookfield-Website'
-                },
-                {
-                    id:1,
-                    title: 'PHP Showcase',
-                    subTitle: 'Website showing various PHP functions',
-                    imgSrc: phpShowcase,
-                    link: '',
-                    code: 'https://github.com/Rakib98/PHP-Showcase'
-                },
-                {
-                    id:2,
-                    title: 'Walsall Tourism',
-                    subTitle: 'Fictional website for a tourism comapny',
-                    imgSrc: walsall,
-                    link: 'https://rakib-walsalltourism.netlify.com/',
-                    code: 'https://github.com/Rakib98/WalsallTourism'
-                },
-                {
-                    id:3,
-                    title: 'Pizza Ordering App',
-                    subTitle:'Program built in C#, for a fictional pizza company',
-                    imgSrc: pizza,
-                    link:'',
-                    code:'https://github.com/Rakib98/PizzaOrdering'
-                }
-            ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 0,
+          title: "Brookfield Garden & Pet Centre",
+          subTitle: "Website for a fictional gaden and pet store",
+          imgSrc: brookfield,
+          link: "https://rakib-brookfield.netlify.com",
+          code: "https://github.com/Rakib98/Brookfield-Website"
+        },
+        {
+          id: 1,
+          title: "PHP Showcase",
+          subTitle: "Website showing various PHP functions",
+          imgSrc: phpShowcase,
+          link: "",
+          code: "https://github.com/Rakib98/PHP-Showcase"
+        },
+        {
+          id: 2,
+          title: "Walsall Tourism",
+          subTitle: "Fictional website for a tourism comapny",
+          imgSrc: walsall,
+          link: "https://rakib-walsalltourism.netlify.com/",
+          code: "https://github.com/Rakib98/WalsallTourism"
+        },
+        {
+          id: 3,
+          title: "Pizza Ordering App",
+          subTitle: "Program built in C#, for a fictional pizza company",
+          imgSrc: pizza,
+          link: "",
+          code: "https://github.com/Rakib98/PizzaOrdering"
+        },
+        {
+          id: 4,
+          title: "Lyrics Search App",
+          subTitle: "React application that fetches data from an API.",
+          imgSrc: lyricsApp,
+          link: "https://rakib-lyrics-search.netlify.com",
+          code: "https://github.com/Rakib98/search-lyrics-songs"
         }
-    }
+      ]
+    };
+  }
 
-
-    /* handleCardClick = (id, card) => {
+  /* handleCardClick = (id, card) => {
 
         let items = [...this.state.items];
 
@@ -71,23 +78,27 @@ class Carousel extends React.Component {
     }
  */
 
-    makeItems = (items) => {
-        return items.map(item => {
-            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
-        })
-    }
+  makeItems = items => {
+    return items.map(item => {
+      return (
+        <Card
+          item={item}
+          click={e => this.handleCardClick(item.id, e)}
+          key={item.id}
+        />
+      );
+    });
+  };
 
-
-    render() {
-        return(
-            <Container fluid={true}>
-                <Row className="col-md-10 offset-md-1">
-                    {this.makeItems(this.state.items)}
-                </Row>
-            </Container>
-        );
-    }
-
+  render() {
+    return (
+      <Container fluid={true}>
+        <Row className="col-md-10 offset-md-1">
+          {this.makeItems(this.state.items)}
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Carousel;
