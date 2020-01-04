8f900e654cbd4239031b1f6832bc08db9b976453
Rakib <rakibabdur98@gmail.com>
2020-01-04 16:18:31 +0000

    changed about section

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,43 +1,46 @@
-import React from 'react';
-import Hero from '../components/Hero';
-import Content from '../components/Content';
+import React from "react";
+import Hero from "../components/Hero";
+import Content from "../components/Content";
 
 function AboutPage(props) {
-    
-    return (
-        <div>
-            <Hero title={props.title}/>
-            <Content>
-                <p>Hi, my name is Rakib Abdur, and I am a student an undergraduate in Software and Web Development. I would like to start working as a fullstack developer, with a bit more emphasize on the front-end. For this reason, I started with React, which has been used to make this website. Every web-dev needs a portfolio, so I decided to create one in React; so that I can use it to both show my skills with React, as well as use it to showcase myself, and future projects.
-                In the next months, I should upload more projects, using React, or Angular.
-                As said before, I am interested in fullstack, so for the backend I am learned a bit of PHP and MySQL. I saw that NodeJS is very popular at the moment, so I want to learn Node, and use it for dealing with requests and more.
-                You can check my CV in here: <a href="https://drive.google.com/open?id=1DBO7lcSeBx4tHR7dJPIDmd_rj3DMWwMaJkooEvLtut4" rel="noopener noreferrer" target="_blank">CV</a>
-                </p>
+  return (
+    <div>
+      <Hero title={props.title} />
+      <Content>
+        <p>
+          Hi, my name is Rakib Abdur, and I am a software developer. During my
+          past education I learned technologies such as HTML/CSS, JavaScript,
+          PHP, MySQL and C#. During my freetime, I expanded on these
+          technologies, by following tutorials, and creating projects. By myself
+          I learned Bootstrap, Materialize CSS and I am currently learning
+          ReactJS. My skills are mainly targeted for the front-end, but I am
+          interested in fullstack. I am currently learning Java, with emphasis
+          on Android. You can check my CV in here:{" "}
+          <a
+            href="https://drive.google.com/open?id=1DBO7lcSeBx4tHR7dJPIDmd_rj3DMWwMaJkooEvLtut4"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            CV
+          </a>
+        </p>
 
-                <p>Check some of the work I have done at my last dev job:
-                </p>
-                    <ul>
-                        <li><a href="https://www.planitglobal.co.uk/" rel="noopener noreferrer" target="_blank">Planit global main page</a></li>
-                        <li><a href="https://www.planitglobal.co.uk/en/explore/infoverse" rel="noopener noreferrer" target="_blank">Infoverse</a></li>
-                        <li> <a href="https://www.planitglobal.co.uk/en/info/business" rel="noopener noreferrer" target="_blank">Business section</a></li>
-                        <li> <a href="https://www.planitglobal.co.uk/en/info/explorer" rel="noopener noreferrer" target="_blank">Explorer section</a></li>
-                        <li> <a href="https://www.planitglobal.co.uk/en/info/education" rel="noopener noreferrer" target="_blank">Education section</a></li>
-                    </ul>
-                <p>
-                    I worked on these pages using HTML/CSS, JS and Ruby on Rails.
-                </p>
-            </Content>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-        </div>
-    );
-
+        <p>
+          I worked as a front-end developer at PLANit Global, where I created
+          new pages that followed the mock designs, and I added new features to
+          their website.
+        </p>
+      </Content>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
 }
 
 export default AboutPage;
